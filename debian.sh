2ed1790b4660671343a13e79f4eef6b81978fc82
kolama <85615694+kolamahost@users.noreply.github.com>
2023-06-08 22:13:45 +0300

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,121 @@
+HOME="/home/container"
+HOMEA="$HOME/linux/.apt"
+STAR1="$HOMEA/lib:$HOMEA/usr/lib:$HOMEA/var/lib:$HOMEA/usr/lib/x86_64-linux-gnu:$HOMEA/lib/x86_64-linux-gnu:$HOMEA/lib:$HOMEA/usr/lib/sudo"
+STAR2="$HOMEA/usr/include/x86_64-linux-gnu:$HOMEA/usr/include/x86_64-linux-gnu/bits:$HOMEA/usr/include/x86_64-linux-gnu/gnu"
+STAR3="$HOMEA/usr/share/lintian/overrides/:$HOMEA/usr/src/glibc/debian/:$HOMEA/usr/src/glibc/debian/debhelper.in:$HOMEA/usr/lib/mono"
+STAR4="$HOMEA/usr/src/glibc/debian/control.in:$HOMEA/usr/lib/x86_64-linux-gnu/libcanberra-0.30:$HOMEA/usr/lib/x86_64-linux-gnu/libgtk2.0-0"
+STAR5="$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/modules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/immodules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/printbackends"
+STAR6="$HOMEA/usr/lib/x86_64-linux-gnu/samba/:$HOMEA/usr/lib/x86_64-linux-gnu/pulseaudio:$HOMEA/usr/lib/x86_64-linux-gnu/blas:$HOMEA/usr/lib/x86_64-linux-gnu/blis-serial"
+STAR7="$HOMEA/usr/lib/x86_64-linux-gnu/blis-openmp:$HOMEA/usr/lib/x86_64-linux-gnu/atlas:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-miners-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/lapack:$HOMEA/usr/lib/x86_64-linux-gnu/gedit"
+STARALL="$STAR1:$STAR2:$STAR3:$STAR4:$STAR5:$STAR6:$STAR7"
+export LD_LIBRARY_PATH=$STARALL
+export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
+export BUILD_DIR=$HOMEA
+
+bold=$(echo -en "\e[1m")
+nc=$(echo -en "\e[0m")
+lightblue=$(echo -en "\e[94m")
+lightgreen=$(echo -en "\e[92m")
+clear
+
+echo "
+${bold}${lightgreen}========================================================================
+                                                                                                  
+${bold}${lightblue}         KOLAMA.HOST DEBIAN VM
+${bold}${lightblue}         KOLAMA.HOST VM INSTALLER
+                                                                                                                
+${bold}${lightgreen}========================================================================
+ "
+ 
+echo "${nc}"
+
+if [[ -f "./installed" ]]; then
+    echo "${bold}${lightgreen}==> Started ${lightblue}VM${lightgreen} <=="
+    function runcmd1 {
+        printf "${bold}${lightgreen}root${nc}@${lightblue}vm${nc}:~ "
+        read -r cmdtorun
+        ./dist/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd
+    }
+    function runcmd {
+        printf "${bold}${lightgreen}root${nc}@${lightblue}vm${nc}:~ "
+        read -r cmdtorun
+        ./dist/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd1
+    }
+    runcmd
+else
+    echo "Downloading files for PteroVM"
+    curl -sSLo ngrok.tar.gz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz >/dev/null 2>err.log
+    echo -ne '#                   (5%)\r'
+    curl -sSLo ptero-vm.zip https://media.githubusercontent.com/media/afnan007a/PteroVM-Files/master/files/ptero-vm.zip >/dev/null 2>err.log
+    echo -ne '##                  (10%)\r'
+    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
+    echo -ne '####                (20%)\r'
+    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
+    echo -ne '#####               (25%)\r'
+    curl -sSLo nvm-install.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh >/dev/null 2>err.log
+    echo -ne '######              (30%)\r'
+    chmod +x unzip >/dev/null 2>err.log
+    export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
+    tar -xvf ngrok.tar.gz >/dev/null 2>err.log
+    echo -ne '#######              (35%)\r'
+    ./unzip ptero-vm.zip >/dev/null 2>err.log
+    echo -ne '########             (40%)\r'
+    ./unzip root.zip
+    tar -xf root.tar.gz >/dev/null 2>err.log
+    echo -ne '#########            (45%)\r'
+    chmod +x ./dist/proot >/dev/null 2>err.log
+    echo -ne '##########           (50%)\r'
+    chmod +x ngrok >/dev/null 2>err.log
+    echo -ne '###########          (55%)\r'
+    chmod +x gotty >/dev/null 2>err.log
+    echo -ne '############         (60%)\r'
+    chmod +x nvm-install.sh >/dev/null 2>err.log
+    echo -ne '#############        (65%)\r'
+    touch .bashrc
+    bash nvm-install.sh >/dev/null 2>err.log
+    echo -ne '#############        (70%)\r'
+    rm -rf ptero-vm.zip >/dev/null 2>err.log
+    rm -rf root.zip >/dev/null 2>err.log
+    rm -rf root.tar.gz >/dev/null 2>err.log
+    rm -rf ngrok.zip >/dev/null 2>err.log
+    rm -rf nvm-install.sh >/dev/null 2>err.log
+    echo -ne '##############       (75%)\r'
+
+    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py" "clear")
+
+    for cmd in "${cmds[@]}"; do
+        ./dist/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
+    done
+    echo -ne '####################(100%)\r'
+    echo -ne '\n'
+    touch installed
+    clear
+    
+    echo "
+${bold}${lightgreen}========================================================================
+                                                                                                  
+${bold}${lightblue}         KOLAMA.HOST DEBIAN VM
+${bold}${lightblue}         KOLAMA.HOST VM STARTING
+                                                                                                                
+${bold}${lightgreen}========================================================================
+ "
+ 
+echo "${nc}"
+    
+    echo "${bold}${lightgreen}==> Started ${lightblue}VM${lightgreen} <=="
+    function runcmd1 {
+        printf "${bold}${lightgreen}root${nc}@${lightblue}vm${nc}:~ "
+        read -r cmdtorun
+        ./dist/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd
+    }
+    function runcmd {
+        printf "${bold}${lightgreen}root${nc}@${lightblue}vm${nc}:~ "
+        read -r cmdtorun
+        ./dist/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd1
+    }
+    runcmd
+fi
